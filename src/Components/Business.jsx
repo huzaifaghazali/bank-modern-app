932284701c2eae57e1494140fa420422c3a71363
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-06-27 20:13:10 +0500

    Create Button component

--- src/Components/Business.jsx
+++ src/Components/Business.jsx
@@ -29,10 +29,10 @@ const Business = () => {
   return (
     <section id='features' className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h4 className={styles.heading2}>
+        <h2 className={styles.heading2}>
           You do the business, <br className='sm:block hidden' /> we’ll handle
           the money.
-        </h4>
+        </h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           With the right credit card, you can improve your financial life by
           building credit, earning rewards and saving money. But with hundreds
